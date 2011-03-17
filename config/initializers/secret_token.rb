7cfea93dd21d7749b85ecd42f8ec9149209d37d7
dgardner88 <david.gardner@students.olin.edu>
2011-03-17 11:16:38 -0400

    Rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnapdragonRails::Application.config.secret_token = '44c637877fb8bd001e38469fa0584fda1eabb4e4c10c1c03c91e9ec58925f98df54fad311bbda9fdeac630b4d2c1eb0267d6550a970d3e7c172d46acfe99882a'
